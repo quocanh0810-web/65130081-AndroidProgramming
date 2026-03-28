3d10240fc3c16044917cc17f7da2b9056c1d103d
Quoc Anh <quocanh0810mwg@gmail.com>
2026-03-28 13:22:49 +0700

    BTH_9

---
+++ AndroidStudioProject/BTH_9/build.gradle.kts
@@ -0,0 +1,7 @@
+import org.gradle.kotlin.dsl.provideDelegate
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+}
+
