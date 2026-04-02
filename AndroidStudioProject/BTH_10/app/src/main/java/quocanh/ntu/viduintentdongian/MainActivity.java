ed65d902ffcbc8f43baa81af27416e3951265b1d
Quoc Anh <quocanh0810mwg@gmail.com>
2026-04-02 16:39:39 +0700

    BaiTH10_ViduIntentDonGian

---
+++ AndroidStudioProject/BTH_10/app/src/main/java/quocanh/ntu/viduintentdongian/MainActivity.java
@@ -0,0 +1,27 @@
+package quocanh.ntu.viduintentdongian;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+        public void ChuyenManHinh(View v) {
+            Intent iManHinhKhac = new Intent(this, SubActivityOne.class);
+            startActivity(iManHinhKhac);
+        }
+    }
+
+
