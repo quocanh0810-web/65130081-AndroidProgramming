1c91b2b11cc0bea0c60d387e0285c962a02b26f9
Quoc Anh <quocanh0810mwg@gmail.com>
2026-04-20 08:04:59 +0700

    COMMIT là LẦN THỨ 2

--- ThiGK_LeQuocAnh_65130081/app/src/main/java/thigk2/lequocanh/thigiuaki/ChucNang1Fragment.java
+++ ThiGK_LeQuocAnh_65130081/app/src/main/java/thigk2/lequocanh/thigiuaki/ChucNang1Fragment.java
@@ -7,41 +7,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChucNang1Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ChucNang1Fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    EditText editText_Vnd;
+    EditText editText_DoLa;
+    Button btnChuyen;
     public ChucNang1Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChucNang1Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static ChucNang1Fragment newInstance(String param1, String param2) {
         ChucNang1Fragment fragment = new ChucNang1Fragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +32,35 @@ public class ChucNang1Fragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chuc_nang1, container, false);
+        View viewCau1 = inflater.inflate(R.layout.fragment_chuc_nang1, container, false);
+        // Tìm điều khiển trong view này
+        editText_Vnd = viewCau1.findViewById(R.id.editTienVN);
+        editText_DoLa = viewCau1.findViewById(R.id.editTienDL);
+        btnChuyen = viewCau1.findViewById(R.id.btnDoi);
+        btnChuyen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String duLieuVnd = editText_Vnd.getText().toString();
+                String duLieuDola = editText_DoLa.getText().toString();
+                //
+                if(!duLieuVnd.isEmpty())
+                {
+                    Toast.makeText(viewCau1.getContext(),"vnd->dola",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(viewCau1.getContext(),"dola->vnd",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        return viewCau1;
     }
 }
--- ThiGK_LeQuocAnh_65130081/app/src/main/java/thigk2/lequocanh/thigiuaki/MainActivity.java
+++ ThiGK_LeQuocAnh_65130081/app/src/main/java/thigk2/lequocanh/thigiuaki/MainActivity.java
@@ -1,24 +1,51 @@
 package thigk2.lequocanh.thigiuaki;
 
+import android.net.wifi.hotspot2.pps.HomeSp;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                int itemId = item.getItemId();
+                if (itemId == R.id.nav_home) {
+                    selectedFragment = new HomeFragment();
+                } else if (itemId == R.id.navigation_chucnang1) {
+                    selectedFragment = new ChucNang1Fragment();
+                } else if (itemId == R.id.navigation_chucnang2) {
+                    selectedFragment = new ChucNang2Fragment();
+                } else if (itemId == R.id.navigation_chucnang3) {
+                    selectedFragment = new ChucNang3Fragment();
+                } else if (itemId == R.id.navigation_chucnang4) {
+                    selectedFragment = new ChucNang4Fragment();
+                }
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentSpace, selectedFragment).commit();
+                }
+                return true;
+
+            }
         });
     }
 }
