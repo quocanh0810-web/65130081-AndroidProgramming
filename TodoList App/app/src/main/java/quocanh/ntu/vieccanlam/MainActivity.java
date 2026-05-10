fe3b7164e19877f8cff480167e995d27efbb4c21
Quoc Anh <quocanh0810mwg@gmail.com>
2026-05-10 17:29:18 +0700

    Hoàn Thành Phần 2

--- TodoList App/app/src/main/java/quocanh/ntu/vieccanlam/MainActivity.java
+++ TodoList App/app/src/main/java/quocanh/ntu/vieccanlam/MainActivity.java
@@ -1,24 +1,53 @@
 package quocanh.ntu.vieccanlam;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.FirebaseApiNotAvailableException;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<TASKS> lstVCL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        // Tạo kết nối đến cơ sở dữ liệu
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = database.getReference("TASKS");
+        // Lắng nghe và xử lý
+        lstVCL = new ArrayList<TASKS>();
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // lấy dữ liệu từ biến snapshot
+                for(DataSnapshot obj: snapshot.getChildren()) {
+                    TASKS task = obj.getValue(TASKS.class);
+                    lstVCL.add(task);
+                    Log.w("VCL app", "Tên việc cần làm: " + task.getName());
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
     }
 }
